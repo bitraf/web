deeb0869eaf8782c46c4407b0875f79fb354f087
Morten Hustveit <morten.hustveit@gmail.com>
2012-08-21 12:49:54 +0200

    * Use ordinary paragraph styles * Menu links now share colors with other links * Try to balance vertical margins on top image after black line went away

--- index.php
+++ index.php
@@ -8,10 +8,6 @@
 .info {
   margin: 10px 0;
 }
-.info p { margin: 1em 0; }
-.info :first-child { font-weight: bold; margin: 0; }
-.info :nth-child(2) { width: 300px; margin-right: 20px; float: left; }
-.info :nth-child(3), .info :nth-child(4) { margin-left: 320px; }
 
 </style>
 <body onload="initialize()" onunload="GUnload()">
@@ -27,15 +23,13 @@
     <div class="button meetup-button"><a href='http://meetup.com/bitraf/'><img src='/images/logo_tilt.gif' alt='Meetup'></a></div>
   </header>
 
-  <div style="overflow: hidden; height: 350px;"><img style="margin-top:-35px;" src='/images/bitraf-rom-960.jpg' alt='' title='Bitrafs lokale, August 2012'></div>
-  <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+  <div><img src='/images/bitraf-rom-960-350.jpg' alt='' title='Bitrafs lokale, August 2012'></div>
 
   <div class=info>
-  <p class=start>Bitraf er et <a href="http://en.wikipedia.org/wiki/Hackerspace">hackerspace</a>
-  i Oslo som er åpent for hvem som helst 7 dager i uka.
-
-  <p>Kom innom om du er interessert i programmering, digital kunst, elektronikk,
-   fri programvare, spillutvikling eller andre liknende aktiviteter.
+  <p class=start><strong>Bitraf er et <a href="http://en.wikipedia.org/wiki/Hackerspace">hackerspace</a>
+  i Oslo som er åpent for hvem som helst 7 dager i uka.</strong>  Kom innom om du er
+  interessert i program&shy;mering, digital kunst, elektronikk,
+  fri programvare, spillutvikling eller andre liknende aktiviteter.
 
   <p>Jobb med egne prosjekter i et sosialt miljø, hold eller
   delta på en workshop, et game jam, hackathon, bug squashing party eller
@@ -43,7 +37,7 @@
   startups som ønsker å jobbe i et slikt miljø.
 
   <p >Bitraf holder til i 128.5m² lokaler <a href=#kart>ved Alexander Kiellands plass</a>.
-  
+
   <!--p>Bitraf er et gjørokrati; den som gjør noe bestemmer hvordan det blir. Vil
   du holde et arrangement, bygge noe eller forbedre noe i lokalet, er det bare
   å gjøre det.-->
@@ -55,16 +49,16 @@
       <th><p>Fredag 24. august, 19:00
       <td style='width: 700px'>
         <p><a href='http://www.meetup.com/bitraf/events/77720112/'>Åpen kveld på Bitraf</a>
-        <p>Er du nysgjerrig på hva et Hackerspace er og hva man gjør der? Denne 
+        <p>Er du nysgjerrig på hva et Hackerspace er og hva man gjør der? Denne
         kvelden holder Bitraf åpent hus og alle er velkommen til å hilse på.
-        <p>Det blir grilling (etter førstemann til mølla-prinsippet) så kom 
-        gjerne rett fra jobb. Ta med venner og bekjente og besøk Oslos største 
+        <p>Det blir grilling (etter førstemann til mølla-prinsippet) så kom
+        gjerne rett fra jobb. Ta med venner og bekjente og besøk Oslos største
         Hackerspace!
     <tr>
       <th><p>Lørdag 25. august, 09:00
       <td style='width: 700px'>
         <p><a href='http://www.meetup.com/bitraf/events/76406612/'>Game Jam: Ludum Dare 24</a>
-        <p>Ludum Dare er en online-arrangert spillutviklingshelg, hvor man 
+        <p>Ludum Dare er en online-arrangert spillutviklingshelg, hvor man
         konkurrer i å lage et spill på 48 timer med et gitt tema.
     <tr>
   </table>
@@ -78,7 +72,7 @@
   <h2 id=kart>Kart</h2>
 
   <p><div id='map-canvas' style='width: 960px; height: 600px'></div>
-  <script> 
+  <script>
   function initialize()
   {
     var mapcenter = new google.maps.LatLng(59.927767, 10.75245);
@@ -89,7 +83,7 @@
     var overlay = new google.maps.GroundOverlay("http://scripts.bitraf.no/images/map-overlay.png", overlayBounds);
     overlay.setMap(map);
   }
-  </script> 
+  </script>
 
   <p>De hvite strekene viser sluttspurten når du kommer fra ulike retninger. Den gule prikken markerer inngangsdøren, som ligger i en bakgård hvor det finnes en 30 meter høy mursteinspipe og en barnehage. Over døren står teksten «24», og ved ringeklokken står det «Bitraf (1. etg)». Bygningsarbeidene som vises i Googles bilder er avsluttet, og er nå boligblokker.
 
@@ -147,11 +141,11 @@
 
   <h2>Priser</h2>
 
-  <p>Det er gratis å være på Bitraf. Lokalet er likevel betalt for 
-av foreningens medlemmer og vi oppfordrer besøkende til å melde seg inn. 
+  <p>Det er gratis å være på Bitraf. Lokalet er likevel betalt for
+av foreningens medlemmer og vi oppfordrer besøkende til å melde seg inn.
 Medlemmer får benytte foreningens forbruksvarer &ndash; for eksempel elektronikkomponenter &ndash;
-samt egen nøkkel til lokalet slik at du kan komme inn når det ikke er noen 
-andre til stede. Næringsdrivende har anledning til å leie 
+samt egen nøkkel til lokalet slik at du kan komme inn når det ikke er noen
+andre til stede. Næringsdrivende har anledning til å leie
 kontorplass.
 </p>
 
