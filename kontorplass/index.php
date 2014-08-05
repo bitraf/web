874009f728ee7812da1e2afb54cee03d549c0052
jensa <jenschr@gmail.com>
2014-08-05 07:21:54 -0400

    La til liste over kontorbrukere og endret mail til daglig leder

--- kontorplass/index.php
+++ kontorplass/index.php
@@ -32,12 +32,86 @@
       <a href='http://maps.google.no/maps?q=youngs+gate+6&amp;ie=UTF8&amp;hq=&amp;hnear=Youngs+gate+6,+Sentrum,+0181+Oslo&amp;gl=no&amp;ll=59.914695,10.750763&amp;spn=0.001029,0.002411&amp;t=m&amp;z=19&amp;vpsrc=6'>Youngs gate 6</a> ved Youngstorget.
       </p>
       <p>
-      Vi leier i tillegg ut kontorplasser. Én plass koster <strong>1500 kroner + MVA per
+      Vi leier i tillegg ut kontorplasser. En plass koster <strong>1500 kroner + MVA per
       måned</strong>, og gir tilgang til lokalet 24/7, samt ubegrenset bruk av vår 100/100&nbsp;Mbps Internet-forbindelse.
       </p>
-      <p>Ta kontakt med Alexander Alemayhu på <a href='mailto:alexander@bitraf.no'>alexander@bitraf.no</a> for å avtale visning eller for spørsmål om leiekontrakt.</p>
+      <p>Ta kontakt med Thomas Winther på <a hre='mailto:thomas@bitraf.no'>thomas@bitraf.no</a> for å avtale visning eller for spørsmål om leiekontrakt.</p>
   </div>
-<!-- This looks broken.
+  <div>
+  <p>Liste over kontorbrukere pr August 2014:</p>
+      <table style="fullWidths">
+      	<tr>
+      		<th scope="col">Navn, Firma</th>
+      		<th scope="col">Telefon</th>
+      		<th scope="col">Web</th>
+      		</tr>
+      	<tr>
+      		<td scope="col">Tormod Berge</td>
+      		<td scope="col">9936 0205</td>
+      		<td scope="col"><a href="http://tormodberge.tumblr.com">Web</a></td>
+      		</tr>
+      	<tr>
+      		<td scope="col">Jens Chr Brynildsen (Jensa), Intopublish</td>
+      		<td scope="col">9069 1786</td>
+      		<td scope="col"><a href="http://flashgamer.com">Web</a>, <a href="http://twitter.com/jenschr">Twitter</a></td>
+      		</tr>
+      	<tr>
+      		<td scope="col">Jens Dyvik, Dyvik Design</td>
+      		<td scope="col">9766 9704</td>
+      		<td scope="col"><a href="http://www.dyvikdesign.com">Web</a></td>
+      		</tr>
+      	<tr>
+      		<td scope="col">Lars Jansøn Engvik, Mienko</td>
+      		<td scope="col">4120 3295</td>
+      		<td scope="col"><a href="http://twitter.com/mienkono">Twitter</a>, <a href="http://mienko.no">Web</a></td>
+      		</tr>
+      	<tr>
+      		<td scope="col">Henrik Falla</td>
+      		<td scope="col">9752 5995</td>
+      		<td scope="col"><a href="http://henrikfalla.com">Web</a></td>
+      		</tr>
+      	<tr>
+      		<td scope="col">Kristoffer Follestad, Cantus Studio</td>
+      		<td scope="col">9715 6620</td>
+      		<td scope="col"><a href="http://www.cantusstudio.com">Web</a></td>
+      		</tr>
+      	<tr>
+      		<td scope="col">Joakim Henriksen</td>
+      		<td scope="col">4190 7240</td>
+      		<td scope="col">&nbsp;</td>
+      		</tr>
+      	<tr>
+      		<td scope="col">Bjørnar Lie, Elsys</td>
+      		<td scope="col">9582 6274</td>
+      		<td scope="col">&nbsp;</td>
+      		</tr>
+      	<tr>
+      		<td scope="col">Wajid Malik</td>
+      		<td scope="col">9887 3083</td>
+      		<td scope="col">&nbsp;</td>
+      		</tr>
+      	<tr>
+      		<td scope="col">Just Thomas Misund, Mienko</td>
+      		<td scope="col">4668 1747</td>
+      		<td scope="col"><a href="http://twitter.com/mienkono">Twitter</a>, <a href="http://mienko.no">Web</a></td>
+      		</tr>
+      	<tr>
+      		<td scope="col">Helene Roberts</td>
+      		<td scope="col">4826 2300</td>
+      		<td scope="col">&nbsp;</td>
+      		</tr>
+      	<tr>
+      		<td scope="col">Erik Wester</td>
+      		<td scope="col">9772 4216</td>
+      		<td scope="col"><a href="http://www.erikwester.com">Web</a></td>
+      		</tr>
+      	<tr>
+      		<td scope="col">Thomas Winther</td>
+      		<td scope="col">9367 5495</td>
+      		<td scope="col"><a href="http://thomaswinther.no">Web</a></td>
+      		</tr>
+      	</table>
+  </div>
   <h2>Lokalene</h2>
   <h3>Hovedrom, med enkel kjøkkenkrok og toalett</h3>
   <div class="panorama">
@@ -55,7 +129,6 @@
         offset="500,230"
         displaysize="960,600"/>
   </div>
--->
 </div>
 </body>
 
