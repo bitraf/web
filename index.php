2a4f043732bad6097ca2e5518e616220a5c1312a
Haavard Espeland <haavares@ifi.uio.no>
2015-03-11 16:35:21 -0400

    Add link to getmylink.php

--- index.php
+++ index.php
@@ -284,6 +284,11 @@ Betalende medlemmer kan i tillegg holde arrangementer, så lenge det ikke kollid
               })();
 </script>
 
+  <h2>Endre medlemskap</h2>
+
+  <p>På<a href='https://p2k12.bitraf.no/getmylink.php'>min side</a> kan du endre medlemskapet ditt og sette opp kredittkortbetaling</p>
+  
+
   <h2>Sponsorer</h2>
   <p>
   <a href="http://renholdsdrift.no"><img src="images/renhold.png" alt="Renholds Service Drift"></a>
