f792400a08f039ada14d6dfb259906c70fcf1392
Morten Hustveit <morten.hustveit@gmail.com>
2015-03-12 00:43:47 +0100

    join/index.php: Use HTTP redirect, with link fallback
    Go to /join instead of /join.php

--- join/index.php
+++ join/index.php
@@ -1,5 +1,13 @@
+<?
+header('HTTP/1.1 302 Moved Temporarily');
+header('Location: https://p2k12.bitraf.no/join');
+?>
 <html>
 <head>
-<meta http-equiv="refresh" content="0; url=https://p2k12.bitraf.no/join.php" />
+<title>Redirect</title>
+<meta http-equiv="refresh" content="0; url=https://p2k12.bitraf.no/join" />
 </head>
+<body>
+<a href=https://p2k12.bitraf.no/join'>https://p2k12.bitraf.no/join</a>
+</body>
 </html>
