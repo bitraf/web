e2e1520033a4a93075297456718e986af8d6e78a
Håvard Espeland <haavares@bitraf.no>
2018-05-09 19:49:17 +0000

    Use p2k16 for join

--- join/index.php
+++ join/index.php
@@ -1,13 +1,13 @@
 <?
 header('HTTP/1.1 302 Moved Temporarily');
-header('Location: https://p2k12.bitraf.no/join');
+header('Location: https://p2k16.bitraf.no');
 ?>
 <html>
 <head>
 <title>Redirect</title>
-<meta http-equiv="refresh" content="0; url=https://p2k12.bitraf.no/join" />
+<meta http-equiv="refresh" content="0; url=https://p2k16.bitraf.no" />
 </head>
 <body>
-<a href=https://p2k12.bitraf.no/join'>https://p2k12.bitraf.no/join</a>
+<a href=https://p2k12.bitraf.no/join'>https://p2k16.bitraf.no</a>
 </body>
 </html>
